6c633e3a0040377733625001af11d0cd67373db5
Angristan <stanislas.lange@openmailbox.org>
2015-09-05 21:28:11 +0200

    bash optimisation

--- OpenNIC-cli.sh
+++ OpenNIC-cli.sh
@@ -8,7 +8,6 @@ else
 read ns1 ns2 <<< $(curl -s https://api.opennicproject.org/geoip/ | head -2 | awk '{print $1}')
 chattr -i /etc/resolv.conf #Allow the modification of the file
 sed -i 's|nameserver|#nameserver|' /etc/resolv.conf #Disable previous DNS servers
-echo "nameserver $ns1
-nameserver $ns2" >> /etc/resolv.conf #Set the DNS servers
+echo -e "nameserver $ns1\ nameserver $ns2" >> /etc/resolv.conf #Set the DNS servers
 chattr +i /etc/resolv.conf #Disallow the modification of the file
 fi
