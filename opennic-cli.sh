65f1ea57b1aff482fad774a62d19329a982416cc
Angristan <stanislas.lange@openmailbox.org>
2016-01-01 13:41:30 +0100

    Update opennic-cli.sh

--- opennic-cli.sh
+++ opennic-cli.sh
@@ -4,7 +4,7 @@ then
    echo -e "Use this script as root."
    exit
 else
-#Getting the nearest OpenNIC servers using the geopi API
+#Getting the nearest OpenNIC servers using the geoip API
 read ns1 ns2 <<< $(curl -s https://api.opennicproject.org/geoip/ | head -2 | awk '{print $1}')
 chattr -i /etc/resolv.conf #Allow the modification of the file
 sed -i 's|nameserver|#nameserver|' /etc/resolv.conf #Disable previous DNS servers
